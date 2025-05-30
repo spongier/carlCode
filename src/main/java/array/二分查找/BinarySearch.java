37274777ab63e59d34edd0ccfc9ae5f5a8ad26b7
spongie <shanehxl@gmail.com>
2025-05-30 10:50:04 +0800

    新增：二分查找，吃香蕉、旋转数组场景题

--- src/main/java/array/二分查找/BinarySearch.java
+++ src/main/java/array/二分查找/BinarySearch.java
@@ -100,6 +100,60 @@ class BinarySearch {
         return r;
     }
 
+
+    // lc 875 吃香蕉问题，吃的越快，耗时越小
+    public int minEatingSpeed(int[] piles, int h) {
+        // 题目强调了 h >= piles.length，也就是取数组最大值，就一定能在h小时吃完
+        int max = -1;
+        for(int x: piles)   max = Math.max(max, x);
+
+        // 这样就定下k值的区间[1, maxH]
+        int l = 1, r = max;
+        while (l < r) {
+            int k = l + (r - l) / 2;
+
+            // 计算时间
+            int cost = 0;
+            for(int pile : piles)  cost += (pile % k == 0 ? pile / k : pile / k + 1);
+
+            // 缩小范围，寻找左边界
+            if (cost > h) {
+                l = k + 1;
+            } else if (cost <= h){
+                r = k;
+            }
+        }
+        return l;
+    }
+
+    // lc 33 旋转数组搜索
+    // 调整边界
+    public int searchRotate(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)  return m;
+
+            if (nums[m] >= nums[l]) {
+                // mid 处于左段升序 (需要二次比较，如果target < mid值，并不代表target会出现在左侧，也可能是右段)
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else {
+                // mid 处于右段升序
+                if (nums[m] < target && target <= nums[r]){
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
     public static void main(String[] args){
         BinarySearch test = new BinarySearch();
         test.mySqrt(12);
